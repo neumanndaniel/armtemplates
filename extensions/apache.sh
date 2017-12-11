abdafc15b452f61e524e4b9ddaa130e728fecb77
Daniel Neumann <mail@neumanndaniel.de>
2017-12-11 11:50:03 +0100

    Update apache.sh

--- extensions/apache.sh
+++ extensions/apache.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
 #Install updates
+echo 'Updating operating system...'
 yum update -y
 
 #Configure automated updates
+echo 'Configuring operating system for automatic updates...'
 yum install -y yum-cron
 
+echo 'Checking /etc/yum/yum-cron.conf for correct configuration...'
 if [ $(grep -c 'update_messages = no' /etc/yum/yum-cron.conf) -eq 1 ]
 then
 	sed -i 's/^update_messages = no/update_messages = yes/g' /etc/yum/yum-cron.conf
@@ -30,10 +33,14 @@ else
     echo 'apply_updates value is already set to yes!'
 fi
 
+echo 'Starting yum-cron.service and enable auto start...'
 systemctl start yum-cron.service
 systemctl enable yum-cron.service
 
 #Install Apache and configure it for auto start
+echo 'Installing Apache web server...'
 yum install -y httpd
+
+echo 'Starting httpd.service and enable auto start...'
 systemctl start httpd.service
 systemctl enable httpd.service
