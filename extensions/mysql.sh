f8fc91e8a59767fac62fd922d495acf90514be37
Daniel Neumann <mail@neumanndaniel.de>
2017-12-11 11:50:24 +0100

    Update mysql.sh

--- extensions/mysql.sh
+++ extensions/mysql.sh
@@ -2,11 +2,14 @@
 mysqlPassword=$1
 
 #Install updates
+echo 'Updating operating system...'
 yum update -y
 
 #Configure automated updates
+echo 'Configuring operating system for automatic updates...'
 yum install -y yum-cron
 
+echo 'Checking /etc/yum/yum-cron.conf for correct configuration...'
 if [ $(grep -c 'update_messages = no' /etc/yum/yum-cron.conf) -eq 1 ]
 then
 	sed -i 's/^update_messages = no/update_messages = yes/g' /etc/yum/yum-cron.conf
@@ -31,31 +34,39 @@ else
     echo 'apply_updates value is already set to yes!'
 fi
 
+echo 'Starting yum-cron.service and enable auto start...'
 systemctl start yum-cron.service
 systemctl enable yum-cron.service
 
 #Partitioning data disk, formatting it with ext4 and mount it
+echo 'Getting empty data disk and create primary partition...'
 lsblk > disk.conf
 if [ $(grep -c 'sdc' disk.conf) -eq 1 ]
 then
     echo -e "n\np\n1\n\n\nw" | fdisk /dev/sdc
 fi
 
+echo 'Formatting new primary partition with ext4 file system...'
 mkfs.ext4 -L /mysqldb /dev/sdc1
 
+echo 'Creating mount point and mount the new primary partition...'
 mkdir /mnt/mysqldb
 mount /dev/sdc1 /mnt/mysqldb
 
+echo 'Creating entry in /etc/fstab for the new primary partition...'
 echo "LABEL=/mysqldb /mnt/mysqldb ext4 defaults,nofail 1 2" | tee -a /etc/fstab
 
 #Install MySQL server
+echo 'Installing MySQL database server...'
 wget http://repo.mysql.com/mysql57-community-release-el7-11.noarch.rpm
 rpm -ivh mysql57-community-release-el7-11.noarch.rpm
 
 yum install -y mysql-server
 
-systemctl start mysqld.service
+echo 'Starting mysqld.service and enable auto start...'
+systemctl start mysqld.servicemysq
 systemctl enable mysqld.service
 
+echo 'Changing the root password...'
 temppassword=$(grep 'temporary password' /var/log/mysqld.log|cut -d ':' -f4|cut -d ' ' -f2)
 mysqladmin -u root -p"$temppassword" password "$mysqlPassword"
