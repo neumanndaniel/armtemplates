1a22d18c70d1457d0fc074197ba0e6ff94156f83
Daniel Neumann <mail@neumanndaniel.de>
2017-12-20 16:07:49 +0100

    Update securityTestServer.ps1

--- extensions/securityTestServer.ps1
+++ extensions/securityTestServer.ps1
@@ -1,3 +1,3 @@
 New-Item -Path C:\ -Name "_securityTest" -ItemType Directory -Verbose
 Copy-Item -Path C:\Windows\notepad.exe -Destination C:\_securityTest\ASC_AlertTest_662jfi039N.exe -Verbose
-"ASC_AlertTest_662jfi039N.exe -foo" > C:\_securityTest\runSecurityTest.cmd
+".\ASC_AlertTest_662jfi039N.exe -foo" > C:\_securityTest\runSecurityTest.ps1
