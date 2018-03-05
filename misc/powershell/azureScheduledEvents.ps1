4908df5a1fb60f179d991e3567fe358fa9a2a4b2
Daniel Neumann <mail@neumanndaniel.de>
2018-03-05 16:39:43 +0100

    Create azureScheduledEvents.ps1

---
+++ misc/powershell/azureScheduledEvents.ps1
@@ -0,0 +1,1 @@
+(Invoke-WebRequest -Headers @{'Metadata'='true'} -UseBasicParsing -Uri 'http://169.254.169.254/metadata/scheduledevents?api-version=2017-08-01' -Verbose).Content|ConvertFrom-Json|ConvertTo-Json -Depth 99
