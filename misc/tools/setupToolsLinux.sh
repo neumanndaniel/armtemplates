e364bec2963a3f367e46fb79dabb071ce67bf4ef
Daniel Neumann <mail@neumanndaniel.de>
2018-04-05 09:38:00 +0200

    Update setupToolsLinux.sh

--- misc/tools/setupToolsLinux.sh
+++ misc/tools/setupToolsLinux.sh
@@ -4,7 +4,8 @@
 if [ $(cat /etc/*_version|grep -c 'Ubuntu') -ge 1 ]
 then
     #Azure CLI
-    echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ wheezy main" | sudo tee /etc/apt/sources.list.d/azure-cli.list
+    AZ_REPO=$(lsb_release -cs)
+    echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ $AZ_REPO main" | sudo tee /etc/apt/sources.list.d/azure-cli.list
     sudo apt-key adv --keyserver packages.microsoft.com --recv-keys 52E16F86FEE04B979B07E28DB02C46DF417A0893
     sudo apt-get install apt-transport-https
     sudo apt-get update && sudo apt-get install azure-cli
@@ -13,9 +14,9 @@ then
     curl https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
     if [ $(cat /etc/*_version|grep -c 'Ubuntu 16.04') -ge 1 ]
     then
-        curl https://packages.microsoft.com/config/ubuntu/16.04/prod.list | sudo tee /etc/apt/sources.list.d/microsoft.list
+        sudo curl https://packages.microsoft.com/config/ubuntu/16.04/prod.list | sudo tee /etc/apt/sources.list.d/microsoft.list
     else
-        curl https://packages.microsoft.com/config/ubuntu/17.04/prod.list | sudo tee /etc/apt/sources.list.d/microsoft.list
+        sudo curl https://packages.microsoft.com/config/ubuntu/17.04/prod.list | sudo tee /etc/apt/sources.list.d/microsoft.list
     fi
     sudo apt-get update
     sudo apt-get install -y powershell
