87b3277b8a42f4373a73c490b8e3f23fe8832d4e
Daniel Neumann <neumanndanielde@outlook.com>
2018-06-25 13:40:16 +0200

    Update setupToolsWindows.ps1

--- misc/tools/setupToolsWindows.ps1
+++ misc/tools/setupToolsWindows.ps1
@@ -1,15 +1,15 @@
 Set-Location $PSScriptRoot
 Clear-Host
 
-Write-Verbose 'STEP 1: Install or update AzureRM PowerShell module:' -Verbose
-Write-Output 'STEP 1: Install or update AzureRM PowerShell module:'
+Write-Verbose 'STEP 1: Install or update AzureRm.Netcore PowerShell module:' -Verbose
+Write-Output 'STEP 1: Install or update AzureRm.Netcore PowerShell module:'
 try {
-    $azureRM = Get-Module -ListAvailable AzureRM -Verbose
+    $azureRM = Get-Module -ListAvailable AzureRm.Netcore -Verbose
     if ($azureRM -eq $null) {
-        Install-Module AzureRM -Force -Verbose -ErrorAction Stop
+        Install-Module AzureRm.Netcore -Force -Verbose -ErrorAction Stop
     }
     else {
-        Update-Module AzureRM -Force -Verbose -ErrorAction Stop
+        Update-Module AzureRm.Netcore -Force -Verbose -ErrorAction Stop
     }
 }
 catch {
@@ -45,7 +45,7 @@ catch {
 Write-Verbose 'STEP 4: Download Azure CLI:' -Verbose
 Write-Output 'STEP 4: Download Azure CLI:'
 try {
-    Invoke-WebRequest -Uri "https://azurecliprod.blob.core.windows.net/msi/azure-cli-latest.msi" -OutFile ./AzureCLI.msi -Verbose -ErrorAction Stop
+    Invoke-WebRequest -Uri "https://aka.ms/installazurecliwindows" -OutFile ./AzureCLI.msi -Verbose -ErrorAction Stop
 }
 catch {
     Write-Host $_
